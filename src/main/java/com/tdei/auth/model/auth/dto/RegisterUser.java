36ff84859db8a5c78c7afc9f8064894c7f2dab87
priya-badmanji <priyadarshini.badmanji@gmail.com>
2023-02-21 15:53:26 +0530

    password policy

--- src/main/java/com/tdei/auth/model/auth/dto/RegisterUser.java
+++ src/main/java/com/tdei/auth/model/auth/dto/RegisterUser.java
@@ -3,8 +3,10 @@ package com.tdei.auth.model.auth.dto;
 import lombok.Data;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 @Validated
@@ -13,9 +15,12 @@ public class RegisterUser {
     private String lastName;
     @NotNull
     @NotEmpty
+    @Email()
     private String email;
     private String phone;
     @NotNull
     @NotEmpty
+    @Pattern(regexp = "^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,})(?=(.*[!@#$%^&*()\\-__+.]){1,}).{8,}$", message = "Password policy not satisfied. " +
+            ">8 characters length, atleast 1 letter in Upper Case, atleast 1 Special Character (!@#$&*()), atleast 1 numeral (0-9)")
     private String password;
 }
