fd651960c3ab9d36c98e1441129209f0cf0b019f
MashB <mahesh_badmanji@yahoo.com>
2024-12-24 15:43:47 +0530

    Password policy update and field length restriction

--- src/main/java/com/tdei/auth/model/common/dto/LoginModel.java
+++ src/main/java/com/tdei/auth/model/common/dto/LoginModel.java
@@ -2,10 +2,10 @@ package com.tdei.auth.model.common.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 @Schema(description = "Describes a login information.")
 @Validated
@@ -13,9 +13,14 @@ import javax.validation.constraints.Pattern;
 public class LoginModel {
     @Schema(required = true, description = "Username.")
     @NotNull
+    @Length(min = 1, max = 255)
     private String username;
     @Schema(required = true, description = "password", example = "Password@123")
     @NotNull
-    @Pattern(regexp = "^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,})(?=(.*[!@#$%^&*()\\-__+.]){1,}).{8,}$", message = "Password policy not satisfied. >8 characters length, atleast 1 letter in Upper Case, atleast 1 Special Character (!@#$&*()), atleast 1 numeral (0-9)")
+//    @Pattern(
+//            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!\"#$%&'()*+,\\-./:;<=>?@\\[\\\\\\]^_`{|}~])(?!.*\\s).{8,255}$",
+//            message = "Invalid credentials."
+//    )
+    @Length(min = 8, max = 255)
     private String password;
 }
